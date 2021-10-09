49d3177530986f06d1dace26c6e9aef8840a23e6
Abdallah <abdallahantony55.aa@gmail.com>
2021-10-09 00:16:36 -0700

    modifed logout response message

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -38,6 +38,6 @@ class AuthController extends Controller
 
     public function logout(){
         auth()->user()->tokens()->delete();
-        return response(['messages'=>'logged out'],200);
+        return response(['message'=>'successfully logged out'],200);
     }
 }
